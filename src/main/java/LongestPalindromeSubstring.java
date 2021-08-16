0e18c515be05ad0a1832017d53f449944e2dc404
satishyc <sryc123@gmail.com>
2021-08-16 21:33:39 +0530

    LongestPalindromeSubstring is updated

--- src/main/java/LongestPalindromeSubstring.java
+++ src/main/java/LongestPalindromeSubstring.java
@@ -1,4 +1,27 @@
 public class LongestPalindromeSubstring{
+/*
+Given a string s, return the longest palindromic substring in s.
+
+
+
+Example 1:
+
+Input: s = "babad"
+Output: "bab"
+Note: "aba" is also a valid answer.
+Example 2:
+
+Input: s = "cbbd"
+Output: "bb"
+Example 3:
+
+Input: s = "a"
+Output: "a"
+Example 4:
+
+Input: s = "ac"
+Output: "a"
+*/
     public String longestPalindrome(String s) {
         String palindrome="";
         int size=s.length();
